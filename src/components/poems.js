b20e85b988e155cb487768abcf740b4b872b653e
Heather <heather.meatherall@ontariotechu.net>
2025-09-06 16:12:48 -0400

    poems links updated

--- src/components/poems.js
+++ src/components/poems.js
@@ -2,48 +2,48 @@ const poems = [
   {
     id: 1,
     title: 'A Line of Women',
-    poem: '/ALineOfWomen.png',
+    poem: '../ALineOfWomen.png',
   },
   {
     id: 2,
     title: 'How To Turn A Haunted Hourse Into A Home',
-    poem: '/HowToTurnAHauntedHouse.jpg',
+    poem: '../HowToTurnAHauntedHouse.jpg',
   },
   {
     id: 3,
     title: 'I Will Not Tell You',
     link: 'https://www.kristenoyer.com/poesie-collaboration',
-    poem: '/IWillNotTellYou.png',
+    poem: '../IWillNotTellYou.png',
   },
   {
     id: 4,
     title: 'Like The Moon',
-    poem: '/LikeTheMoon.png',
+    poem: '../LikeTheMoon.png',
   },
   {
     id: 5,
     title: 'Same To Me',
-    poem: '/SameToMe.png',
+    poem: '../SameToMe.png',
   },
   {
     id: 6,
     title: 'Select All That Apply',
-    poem: '/SelectAllThatApply.png',
+    poem: '../SelectAllThatApply.png',
   },
   {
     id: 7,
     title: 'Something About Pride',
-    poem: '/SomethingAboutPride.jpg',
+    poem: '../SomethingAboutPride.jpg',
   },
   {
     id: 9,
     title: 'There Is Comfort There (After M. Travis Lane)',
-    poem: '/ThereIsComfortThere.png',
+    poem: '../ThereIsComfortThere.png',
   },
   {
     id: 8,
     title: 'You Will Live',
-    poem: '/YouWillLive.jpg',
+    poem: '../YouWillLive.jpg',
   },
 ]
 
